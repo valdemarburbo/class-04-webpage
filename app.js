dd97c5a798769125b6bccc5871e6d78fe1035095
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-06-11 23:25:49 +0100

    Add JS to the page. Add alert, prompt, confirm fuctions. Utilise conditional statement.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-alert("Hello! Welcome to Val's webpage!")
+alert("Hello! Welcome to Val's webpage!");
+
+let nameInput = prompt("What is your name?");
+
+let confirmation = confirm("So your name is " + nameInput + "?");
+
+if (confirmation == true) {
+    alert("Thanks. Welcome, " + nameInput + "!");
+} else {
+    nameInput = prompt("What is your name?");
+
+    confirmation = confirm("So your name is " + nameInput + "?");
+}
